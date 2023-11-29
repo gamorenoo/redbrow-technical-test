2e80b21b92976ca95cf937de16fb18847f762d99
Gustavo Adolfo Moreno Ospino <gustavoamoreno@outlook.com>
2023-11-28 23:24:37 -0500

    Documentación métodos Api y README del Api

--- BackEnd/redbrow-technical-test/redbrow-technical-test.Api/Controllers/UsersController.cs
+++ BackEnd/redbrow-technical-test/redbrow-technical-test.Api/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace redbrow_technical_test.Api.Controllers
     /// Controlador de usuairos
     /// </summary>
     [Authorize]
-    [Route("api/")]
+    [Route("api/user/")]
     public class UsersController : ApiControllerBase
     {
         private readonly ILogger<UsersController> _logger;
@@ -30,36 +30,117 @@ namespace redbrow_technical_test.Api.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Crear usuario
+        /// </summary>
+        /// <param name="createUserCommand"></param>
+        /// <returns>Usuario creado</returns>
+        /// <response code="200">Usuario Creado</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPost]
         public async Task<ActionResult<UserDto>> Create([FromForm] CreateUserCommand createUserCommand)
         {
             return await Mediator.Send(createUserCommand);
         }
 
+        /// <summary>
+        /// Actualizar usuario
+        /// </summary>
+        /// <param name="updateUserCommand"></param>
+        /// <returns>Usuario creado</returns>
+        /// <response code="200">Usuario Actualizado</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut]
         public async Task<ActionResult<UserDto>> Put([FromForm] UpdateUserCommand updateUserCommand)
         {
             return await Mediator.Send(updateUserCommand);
         }
 
+        /// <summary>
+        /// Eliminar usuario
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Bandera que indica si se elimina el usuario o no</returns>
+        /// <response code="200">Usuario Actualizado</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpDelete("{id}")]
         public async Task<ActionResult<bool>> Delete(Guid id)
         {
             return await Mediator.Send(new DeleteUserCommand() { Id = id });
         }
 
+        /// <summary>
+        /// Obtener usuarios
+        /// </summary>
+        /// <returns>lista de usuarios registrados</returns>
+        /// <response code="200">Lista de Usuarios</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpGet]
         public async Task<IEnumerable<UserDto>> Get()
         {
             return await Mediator.Send(new GetAllQuery());
         }
 
+        /// <summary>
+        /// Obtener usuario por Id
+        /// </summary>
+        /// <returns>Usuarios que coincide</returns>
+        /// <response code="200">Usuario</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpGet("{id}")]
         public async Task<UserDto> Get(Guid id)
         {
             return await Mediator.Send(new GetByIdQuery() { Id = id });
         }
 
+        /// <summary>
+        /// Obtener usuarios paginados
+        /// </summary>
+        /// <returns>lista de usuarios registrados en la pagina indicada</returns>
+        /// <response code="200">Usuarios paginados</response>
+        /// <response code="401">No autorizado</response>
+        /// <response code="500">Error interno del servidor</response>
+        /// <response code="400">Request invalido</response>
+        /// <returns></returns>
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpGet("get-paginated")]
         public async Task<PaginatedResult<UserDto>> GetPaginated([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
         {
---
+++ README.md
@@ -0,0 +1,40 @@
+# redbrow-technical-test API
+
+Este proyecto es una API de gestión de usuarios en al cual se pueden realizar operaciones CRUD de usuarios (crear, editar, eliminar y obtener información sobre usuarios)
+El proyecto utiliza Docker y Docker Compose para facilitar la configuración del entorno de desarrollo. El proyecto usa autnticacion con JWT para brindar acceso autorizado 
+a los métodos expuestos.
+
+## 2. Solución
+
+Se toma la decisión de trabajar la solución siguiendo los principios de Clean Architecture en .NET 7. 
+El principal objetivo de esta arquitectura es la capacidad de separar el código en sus diferentes responsabilidades, hacer el código mucho más entendible en el presente y futuro, testeable y fácil de integrar.
+
+
+## 2.1 Tecnologías
+* NET 7 / C#
+* ASP.NET Core 7
+* Sql Server
+* Entity Framework Core 7
+* JWT
+* MediatR
+* AutoMapper
+* Docker
+* Visual studio Comunity 2022
+
+## Requisitos Previos
+Asegúrate de tener Docker y Docker Compose instalados en tu máquina antes de comenzar.
+
+## Instrucciones para Ejecutar APi
+
+### Paso 1: Configurar la Base de Datos
+1. Navega a la carpeta "BackEnd\redbrow-technical-test\docker-image-bd" en el directorio del proyecto.
+2. Ejecuta el siguiente comando para cargar la imagen de Sql Server usando Docker Compose:
+   ```bash
+   docker-compose up -d
+2. Eejutar el script contenido en el archivo llamdo ###redbrow-db.sql para crear la base de datos del proyecto y la tabla de usuarios.
+
+### Paso 2: Ejecucion del proyecto
+1. Una vez se tenga la apicación en visual studio y haya sido ejecutada, se debe generar un token de acceso con los datos Usuario: gustavoamoreno@outlook.com -- > Password: 0123456789, este es el unico usuario parametrizado. Lo anterior se indica en la documentacoin del Swagger
+
+
+## Instrucciones para Ejecutar FRONT
